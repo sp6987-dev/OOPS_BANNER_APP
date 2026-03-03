8e7eb5f6a3c07187a55783fe15a9b26e64adfa3c
sp6987-dev <sp6987@srmist.edu.in>
2026-03-03 17:35:43 +0530

    Add OOPSBannerApp to display ASCII banner

---
+++ OOPS_BANNER_APP.java
@@ -0,0 +1,26 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC2: Display "OOPS" in large ASCII banner format
+ * using '*' and spaces with manual print statements.
+ * 
+ * @author swapnanil
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *****   ***** ");
+        System.out.println("*     * *     * *           * ");
+        System.out.println("*     * *     * *           * ");
+        System.out.println(" *****   *****   *      *****  ");
+    }
+}

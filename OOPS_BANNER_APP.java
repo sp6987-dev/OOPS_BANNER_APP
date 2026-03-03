c1ffb929971e1f7431366144c8e70b7b882a3e1a
sp6987-dev <sp6987@srmist.edu.in>
2026-03-03 17:34:30 +0530

    Add OOPSBannerApp to print 'OOPS' to console

---
+++ OOPS_BANNER_APP.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC1: Print the literal text "OOPS" to the console.
+ * This is the foundational implementation for the OOPS Banner App project.
+ * 
+ * @author swapnanil
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Printing literal text to console
+        System.out.println("OOPS");
+    }
+}
